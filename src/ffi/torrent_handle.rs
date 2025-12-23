f3a9a3ec54bfdc18d6a11e94a5d6c8823d4098c5
pliduino <liduinopedro@gmail.com>
2025-12-23 11:39:48 -0300

    0.1.3 release

--- src/ffi/torrent_handle.rs
+++ src/ffi/torrent_handle.rs
@@ -13,9 +13,10 @@ pub(crate) mod ffi {
         include!("cpp/torrent_handle.h");
         include!("lt-rs/src/ffi/mod.rs.h");
         type InfoHashCpp = crate::ffi::ffi::InfoHashCpp;
+        type PieceIndex = crate::ffi::ffi::PieceIndex;
 
         fn torrent_handle_in_session(handle: &torrent_handle) -> bool;
-        fn torrent_handle_read_piece(handle: &torrent_handle, piece: i32);
+        fn torrent_handle_read_piece(handle: &torrent_handle, piece: PieceIndex);
         fn torrent_handle_status(handle: &torrent_handle) -> UniquePtr<torrent_status>;
         fn torrent_handle_save_resume_data(handle: &torrent_handle, flags: u8);
 
